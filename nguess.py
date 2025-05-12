47d49dfe5d1ab6472fe3aa1d20469c89a9ca8850
jyotaro <jotoro1012@au.com>
2025-05-12 21:50:44 +0900

    課題1

--- nguess.py
+++ nguess.py
@@ -1,11 +1,13 @@
 import random
-
-guess = int(input('guess='))
-print('Your guess is', guess)
+k=0
 answer = random.randint(1, 100)
-if guess == answer :
-    print('Good guess')
-elif guess < answer:
-    print('Too low')
-else:
-    print('Too high')
+while k<1:
+    guess = int(input('guess='))
+    print('Your guess is', guess)
+    if guess == answer :
+        print('Good guess')
+        k=k+1
+    elif guess < answer:
+        print('Too low')
+    else:
+        print('Too high')
